d0c3372afc15eed987096f889dea246f8c854e25
amirhossein_t <amirhossein_t@gmial.com>
2023-02-07 12:47:27 +0330

    Add iconify package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import "../styles/index.css";
+import Iconify from "@iconify/iconify";
+import "./styles/main.scss";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
 
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", () => {
-  alert("clicked");
-});
+import "./button";
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,58 @@
 const path = require("path");
-const htmlWebpack = require("html-webpack-plugin");
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const htmlWebpack = require("html-webpack-plugin");
 const config = {
-  entry: "/src/index.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new htmlWebpack(),
+  ],
+  mode: "development",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: "build/",
   },
 
   module: {
     rules: [
+      // Define Rulesets
       {
         test: /\.js$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", { targets: "defaults" }]],
+        },
+      },
+
+      // Store css files in specific  separare file
+      {
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        test: /\.(css|scss)$/,
+      },
+
+      // Load and optimize images with different formats
+      {
+        test: /\.(jp?g|png|gif)$/,
+
+        type: "asset/resource",
+        generator: {
+          publicPath: "build/assets/images/",
+          outputPath: "assets/images/",
         },
       },
       {
-        test: /\.(sass|scss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-
-      {
-        test: /\.(svg|eot|woff|woff2|ttf)$/,
-        use: ["file-loader"],
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          publicPath: "build/assets/fonts/",
+          outputPath: "assets/fonts/",
+        },
       },
     ],
-    plugins: [new htmlWebpack()],
-    devServer: {
-      port: 9001,
-    },
   },
 };
 
